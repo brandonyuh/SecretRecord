fd1c7df17dd8c0e513957ec85a4565856775fd91
brandonyuh <brandonyuh@gmail.com>
2017-05-21 09:50:30 -0700

    Added taking picture

--- SecretRecord/app/src/main/java/me/brandonyuh/secretrecord/MainActivity.java
+++ SecretRecord/app/src/main/java/me/brandonyuh/secretrecord/MainActivity.java
@@ -1,10 +1,22 @@
 package me.brandonyuh.secretrecord;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.ImageFormat;
+import android.graphics.SurfaceTexture;
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,13 +24,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    private static final int ASK_MULTIPLE_PERMISSION_REQUEST_CODE = 1;
+    private static final int TAKE_PICTURE_PERMISSION = 2;
+    public AppCompatActivity appCompatActivity;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        appCompatActivity = this;
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -40,6 +65,90 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, ASK_MULTIPLE_PERMISSION_REQUEST_CODE);
+
+        TextView takePictureTextView = (TextView) findViewById(R.id.textViewTakePicture);
+        takePictureTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tryToTakePicture();
+            }
+        });
+
+        TextView recordAudioTextVideo = (TextView) findViewById(R.id.textViewRecordAudio);
+        recordAudioTextVideo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(appCompatActivity, "Record audio not implemented", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        TextView textViewRecordVideo = (TextView) findViewById(R.id.textViewRecordVideo);
+        textViewRecordVideo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(appCompatActivity, "Record video not implemented", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        TextView textViewStop = (TextView) findViewById(R.id.textViewStop);
+        textViewStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(appCompatActivity, "Stop not implemented", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    public void tryToTakePicture() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            if (shouldShowRequestPermissionRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA}, TAKE_PICTURE_PERMISSION);
+                return;
+            } else {
+                showMessageOKCancel("You need to allow access to the camera and external storage to take pictures.",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA}, TAKE_PICTURE_PERMISSION);
+                            }
+                        });
+                return;
+            }
+        }
+        takePicture();
+    }
+
+    private void takePicture() {
+        Camera mCamera = Camera.open();
+        try {
+            mCamera.setPreviewTexture(new SurfaceTexture(10));
+        } catch (IOException e1) {
+            //Log.e(Version.APP_ID, e1.getMessage());
+        }
+
+        Camera.Parameters params = mCamera.getParameters();
+        params.setPreviewSize(640, 480);
+        params.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+        params.setPictureFormat(ImageFormat.JPEG);
+        mCamera.setParameters(params);
+        mCamera.startPreview();
+        mCamera.takePicture(null, null, null, new Camera.PictureCallback() {
+            @Override
+            public void onPictureTaken(byte[] data, Camera camera) {
+                if(data != null) {
+                    Calendar calendar = Calendar.getInstance();
+                    String datetime = Long.toString(calendar.getTimeInMillis());
+                    Toast.makeText(appCompatActivity, "Picture taken", Toast.LENGTH_LONG).show();
+                    Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+                    MediaStore.Images.Media.insertImage(getContentResolver(), bitmap, datetime, datetime);
+                }
+                else{
+                    Toast.makeText(appCompatActivity, "NULL!", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 
     @Override
@@ -97,5 +206,18 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
+        new AlertDialog.Builder(this)
+                .setMessage(message)
+                .setPositiveButton("OK", okListener)
+                .setNegativeButton("Cancel", null)
+                .create()
+                .show();
+    }
+
+    public Context getActivity() {
+        return this;
     }
 }
